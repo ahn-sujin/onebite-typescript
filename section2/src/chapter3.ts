ee3b846eefeeedf46f24cc8813a9e458be7520a8
ahn-sujin <asj9674@gmail.com>
2024-12-26 18:12:07 +0900

    section2:객체

---
+++ section2/src/chapter3.ts
@@ -0,0 +1,11 @@
+// object
+// 객체 리터럴 타입
+let user: { id: number; name: string; phone?: string } = {
+  id: 1,
+  name: "안수진",
+  phone: "01055049674",
+};
+
+let config: { readonly apiKey: string } = {
+  apiKey: "asdhbfjksd123skdf",
+};
